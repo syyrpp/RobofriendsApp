5d53a1580ea61f4c47f7dd3de952db332de6ba14
syyrpp <calebjenkins17@yahoo.com>
2019-10-30 11:57:00 -0500

    Updates

---
+++ precache-manifest.1f668ae8c92e7d2c38063f4678730f44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1298b575ee4559a6fa996f0eb84211",
+    "url": "/robofriendsapp/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriendsapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "af10ffec4e6e50c90f5c",
+    "url": "/robofriendsapp/static/css/main.b5b1896e.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriendsapp/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "af10ffec4e6e50c90f5c",
+    "url": "/robofriendsapp/static/js/main.41c44398.chunk.js"
+  },
+  {
+    "revision": "822732cd19262843e144",
+    "url": "/robofriendsapp/static/js/runtime-main.6a9697c2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
